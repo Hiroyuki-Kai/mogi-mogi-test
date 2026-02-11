4a9fe9677b8f7bcff9da363371bcc9d602b232eb
Hiroyuki-Kai <hiroyuki.kai23@gmail.com>
2026-02-11 23:36:55 +0900

    キウイの拡張子修正

--- src/database/seeders/ProductsTableSeeder.php
+++ src/database/seeders/ProductsTableSeeder.php
@@ -18,7 +18,7 @@ class ProductsTableSeeder extends Seeder
             [
                 'name' => 'キウイ',
                 'price' => 800,
-                'image' => 'images/kiwi.jpg',
+                'image' => 'images/kiwi.png',
                 'description' => 'キウイは甘みと酸味のバランスが絶妙なフルーツです。ビタミンCなどの栄養素も豊富のため、美肌効果や疲労回復効果も期待できます。もぎたてフルーツのスムージーをお召し上がりください！',
                 'seasons' => ['秋','冬'],
             ],
